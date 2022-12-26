1ccd9d357797bc87073a1660c02678845987f659
C.Ranjith <cranjithrcb@gmail.com>
2022-12-26 18:51:41 +0530

    Login-and-Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,32 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import PageRender from "./PageRender";
+import { refreshToken } from "./redux/actions/authAction";
 import Home from "./pages/home";
 import Login from "./pages/login";
+import Alert from "./components/alert/Alert";
+import Register from "./pages/register";
+import { GLOBALTYPES } from "./redux/actions/globalTypes";
 function App() {
+  const { auth } = useSelector((state) => state);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshToken());
+  }, [dispatch]);
+
   return (
     //passing the id and page as a parameter and rendering
+
     //pagerender component use useParams()
     <Router>
+      <Alert />
       <input type="checkbox" id="theme" />
       <div className="App">
         <div className="main">
-          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={auth.token ? Home : Login} />
           <Route exact path="/:page" component={PageRender} />
           <Route exact path="/:page/:id" component={PageRender} />
         </div>
--- server.js
+++ server.js
@@ -29,6 +29,7 @@ mongoose.connect(
 const port = process.env.PORT || 5000;
 //routes
 app.use("/api", require("./routes/authRouter"));
+
 app.listen(port, () => {
   console.log("Server is running on port", port);
 });
